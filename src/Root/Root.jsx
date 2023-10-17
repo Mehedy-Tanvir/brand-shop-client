c8ceec27417a1a1af205705f7aa4351d3661ef6b
Mehedy-Tanvir <mehedytanvir451@gmail.com>
2023-10-17 23:24:51 +0600

    project set up and added to github

---
+++ src/Root/Root.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+
+const Root = () => {
+  return (
+    <div>
+      <h1>hello from root</h1>
+      <Outlet></Outlet>
+    </div>
+  );
+};
+
+export default Root;
