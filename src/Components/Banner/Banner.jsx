2f5d424f83e07036d7d6b1d05e518d9f75c2dfb4
Mehedy-Tanvir <mehedytanvir451@gmail.com>
2023-10-18 02:12:48 +0600

    banner section ui improved

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -5,15 +5,16 @@ const Banner = () => {
         className="absolute top-0 left-0 w-full h-full bg-orange-50"
         style={{
           backgroundImage: `url('/banner-bg.jpg')`,
-          transform: "scaleY(-1)",
+          transform: "scaleY(-1) scaleX(-1)",
+          backgroundSize: "cover",
         }}
       >
         <div className="bg-orange-100 bg-opacity-90 hero-overlay"></div>
       </div>
       <div className="relative z-10 flex-col hero-content lg:flex-row-reverse">
-        <img src="/banner-img.png" className="lg:max-w-sm" />
+        <img src="/banner-img.png" className="md:max-w-sm" />
         <div>
-          <h1 className="text-6xl  text-blue-1000 font-bold leading-[80px]">
+          <h1 className="text-5xl text-center lg:text-left leading-[50px] md:text-6xl text-blue-1000 font-bold md:leading-[80px]">
             Explore the <br />
             Future of Innovation!
           </h1>
