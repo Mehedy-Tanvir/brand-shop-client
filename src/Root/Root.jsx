60573cecfd2d77b3627f013e9209d5e4d2cd53f2
Mehedy-Tanvir <mehedytanvir451@gmail.com>
2023-10-18 02:21:56 +0600

    footer section added

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Components/Navbar/Navbar";
+import Footer from "../Components/Footer/Footer";
 
 const Root = () => {
   return (
@@ -8,6 +9,7 @@ const Root = () => {
         <Navbar></Navbar>
       </div>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,10 +4,12 @@ import Home from "../Pages/Home/Home";
 import AddProduct from "../Pages/AddProduct/AddProduct";
 import MyCart from "../Pages/MyCart/MyCart";
 import Login from "../Pages/Login/Login";
+import Error from "../Pages/Error/Error";
 const Routes = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
+    errorElement: <Error></Error>,
     children: [
       {
         path: "/",
