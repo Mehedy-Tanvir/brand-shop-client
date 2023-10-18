4323817e18861c935f22beca5a22f16a20e2efd8
Mehedy-Tanvir <mehedytanvir451@gmail.com>
2023-10-18 22:45:55 +0600

    product card added

---
+++ src/Components/ProductCard/ProductCard.jsx
@@ -0,0 +1,50 @@
+import Rating from "react-rating";
+import { AiFillStar, AiOutlineStar } from "react-icons/ai";
+
+const ProductCard = () => {
+  return (
+    <div className="border-2 border-yellow-500 card w-96 bg-base-100">
+      <figure>
+        <img src="/samsung.jpg" alt="Product" />
+      </figure>
+      <div className="card-body">
+        <p className="text-3xl font-semibold">Samsung</p>
+        <h2 className="text-3xl font-bold card-title">Galaxy S23 Ultra</h2>
+        <div className="flex items-center justify-center">
+          <p className="text-xl font-medium">Rating: 4.5</p>
+          <Rating
+            initialRating="4.5"
+            readonly="true"
+            emptySymbol={
+              <AiOutlineStar className="text-2xl text-yellow-500"></AiOutlineStar>
+            }
+            placeholderSymbol={
+              <img src="assets/images/star-red.png" className="icon" />
+            }
+            fullSymbol={
+              <AiFillStar className="text-2xl text-yellow-500"></AiFillStar>
+            }
+          />
+        </div>
+
+        <div className="grid grid-cols-2">
+          <p className="text-xl font-regular">Type: Phone</p>
+          <p className="text-2xl text-yellow-600 font-regular">
+            Price: $999.99
+          </p>
+        </div>
+
+        <div className="grid grid-cols-2 gap-2">
+          <button className="bg-yellow-500 hover:opacity-90 text-white text-2xl h-[50px] px-[20px] rounded-none">
+            Details
+          </button>
+          <button className="bg-yellow-500 hover:opacity-90 text-white text-2xl h-[50px] px-[20px] rounded-none">
+            Update
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,7 +1,10 @@
+import ProductCard from "../../Components/ProductCard/ProductCard";
+
 const Products = () => {
   return (
     <div>
       <h1>Products</h1>
+      <ProductCard></ProductCard>
     </div>
   );
 };
